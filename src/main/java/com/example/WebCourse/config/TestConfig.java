877732450dc4d2d79f3a26e755e79699bf1ae5f3
Tiagohs199 <tiagohs.199@gmail.com>
2021-02-14 19:26:27 -0300

    payment, One-to-One

--- src/main/java/com/example/WebCourse/config/TestConfig.java
+++ src/main/java/com/example/WebCourse/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.example.WebCourse.entities.Order;
 import com.example.WebCourse.entities.OrderItem;
+import com.example.WebCourse.entities.Payment;
 import com.example.WebCourse.entities.Product;
 import com.example.WebCourse.entities.User;
 import com.example.WebCourse.entities.enums.Category;
@@ -82,6 +83,12 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
+		
 		
 	}
 }
